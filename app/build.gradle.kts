fff6fa90ae75719a987347558f99805e91bb98f0
Rami Hail Amer <ramiha@stud.ntnu.no>
2024-10-27 17:36:44 +0100

    Fix the libs.versions.toml (I change the version of agp and add other necessary things like constraintlayout, recyclerview)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
