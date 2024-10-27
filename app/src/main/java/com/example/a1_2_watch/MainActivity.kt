6e053e88dfd7416d5eecc6053c913bd1fb0b853c
Rami Hail Amer <ramiha@stud.ntnu.no>
2024-10-27 17:47:50 +0100

    (#6) I add the necessary package for our project.

--- app/src/main/java/com/example/a1_2_watch/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.a1_2_watch
-
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-
-}
---
+++ app/src/main/java/com/example/a1_2_watch/ui/HomeActivity.kt
@@ -0,0 +1,7 @@
+package com.example.a1_2_watch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+
+}
