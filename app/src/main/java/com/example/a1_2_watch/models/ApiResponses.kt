f3c086802f355511fd5e2681ded401720814bf35
Rami Hail Amer <ramiha@stud.ntnu.no>
2024-11-03 23:16:38 +0100

    (#documentation) I add documentations to ApiResponses.

--- app/src/main/java/com/example/a1_2_watch/models/ApiResponses.kt
+++ app/src/main/java/com/example/a1_2_watch/models/ApiResponses.kt
@@ -1,13 +1,28 @@
 package com.example.a1_2_watch.models
 
+/**
+ * Response model for a list of movies.
+ *
+ * @property results The list of movies to be returned from the API.
+ */
 data class MovieResponse(
     val results: List<Movie>
 )
 
+/**
+ * Response model for a list of TV shows.
+ *
+ * @property results The list of TV shows to be returned from the API.
+ */
 data class ShowResponse(
     val results: List<Show>
 )
 
+/**
+ * Response model for a list of anime.
+ *
+ * @property data The list of anime to be returned from the API.
+ */
 data class AnimeResponse(
     val data: List<Anime>
 )
