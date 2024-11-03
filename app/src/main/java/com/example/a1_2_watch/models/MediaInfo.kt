18e74848941bb9a84585d1a7dcf58c516999de89
Rami Hail Amer <ramiha@stud.ntnu.no>
2024-11-03 23:04:38 +0100

    (#documentation) I add documentations to MediaInfo

--- app/src/main/java/com/example/a1_2_watch/models/MediaInfo.kt
+++ app/src/main/java/com/example/a1_2_watch/models/MediaInfo.kt
@@ -1,6 +1,15 @@
 package com.example.a1_2_watch.models
 
-
+/**
+ * Data class representing a movie.
+ *
+ * @property id The unique ID of the movie.
+ * @property title The title of the movie.
+ * @property overview The brief description of the movie.
+ * @property poster_path The path to the movie's poster image.
+ * @property vote_average The average rating of the movie.
+ * @property isLiked Indicates if the movie is marked as liked by the user or not.
+ */
 data class Movie(
     val id: Int,
     val title: String?,
@@ -10,6 +19,16 @@ data class Movie(
     var isLiked: Boolean = false
 )
 
+/**
+ * Data class representing a TV show.
+ *
+ * @property id The unique ID of the TV show.
+ * @property name The name of the TV show.
+ * @property overview The brief description of the TV show
+ * @property poster_path The path to the TV show poster image.
+ * @property vote_average The average rating of the TV show.
+ * @property isLiked Indicates if the TV show is marked as liked by the user or not.
+ */
 data class Show(
     val id: Int,
     val name: String?,
@@ -20,6 +39,13 @@ data class Show(
 
 )
 
+/**
+ * Data class representing an anime
+ *
+ * @property id The unique ID of the anime.
+ * @property attributes Additional attributes containing additional details about the anime.
+ * @property isLiked Indicates if the anime is marked as liked by the user or not.
+ */
 data class Anime(
     val id: Int,
     val attributes: Attributes,
